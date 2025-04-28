f0f8dd899d67bfdd4393764492e854f4db5213c4
Henry sipp <hesipp@gmail.com>
2025-04-28 12:02:34 -0500

    Icons

--- .config/hypr/waybar/launch.sh
+++ .config/hypr/waybar/launch.sh
@@ -2,5 +2,5 @@
 killall -q waybar
 while pgrep -x waybar >/dev/null; do sleep 1; done
 
-waybar -c $HOME/.config/hypr/waybar/config -s $HOME/.config/hypr/waybar/style.css
-# env GTK_DEBUG=interactive waybar -c $HOME/.config/hypr/waybar/config -s $HOME/.config/hypr/waybar/style.css
+waybar -c $HOME/.config/hypr/waybar/config.jsonc -s $HOME/.config/hypr/waybar/style.css
+# env GTK_DEBUG=interactive waybar -c $HOME/.config/hypr/waybar/config.jsonc -s $HOME/.config/hypr/waybar/style.css
